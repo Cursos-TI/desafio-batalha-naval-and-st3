355e860239d0dff5e9ac8ebd5d1d6a33380ea2de
Andrews <98049668+and-st3@users.noreply.github.com>
2025-05-27 20:08:01 +0000

    validação implementada

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -21,26 +21,56 @@ int main() {
     int navio2[3] = {3, 3, 3};
     int navio3[3] = {3, 3, 3}; // Navio diagonal
     int navio4[3] = {3, 3, 3}; // Navio diagonal
-
+    
+    
     // Posicionamento dos Navios no Tabuleiro
     // Navio horizontal
-    tabuleiroNaval[3][3] = navio1[0];
-    tabuleiroNaval[3][4] = navio1[1];
-    tabuleiroNaval[3][5] = navio1[2];
-
+    for (int i = 0; i < 3; i++) {
+        int x = 3;
+        if (i >= 0 && i <= 10 && x >= 0 && x < 10) {
+            tabuleiroNaval[x][3 + i] = navio1[i];
+        } else{
+            printf("Erro: Índice fora dos limites do tabuleiro.\n");
+            return -1;
+        }
+    }
+    
     // Navio vertical
-    tabuleiroNaval[5][1] = navio2[0];
-    tabuleiroNaval[6][1] = navio2[1];
-    tabuleiroNaval[7][1] = navio2[2];
+    for (int i = 0; i < 3; i++) {
+        int y = 1;
+        if (i >= 0 && i <= 10 && y >= 0 && y < 10) {
+            tabuleiroNaval[5 + i][y] = navio2[i]; 
+        } else{
+            printf("Erro: Índice fora dos limites do tabuleiro.\n");
+            return -1;
+        }
+    }
 
     // Navios Diagonais
-    for (int x = 0; x < 3; x++) {
-        tabuleiroNaval[5 + x][x + 3] = navio3[x];
+    for (int k = 0; k < 3; k++) {
+        int x = 6 + k;
+        int y = 7 + k;
+        if (x >= 0 && x < 10 && y >= 0 && y < 10) {
+            tabuleiroNaval[x][y] = navio3[k];
+        } else{
+            printf("Erro: Índice fora dos limites do tabuleiro.\n");
+            return -1;
+        }
     }
 
-    for (int y = 0; y < 3; y++) {
-        tabuleiroNaval[6 + y][9 - y] = navio4[y];
-    }
+    
+    for (int k = 0; k < 3; k++) {
+        int x = 5 + k;
+        int y = 3 + k;
+        if (x >= 0 && x < 10 && y >= 0 && y < 10) {
+            tabuleiroNaval[x][8 - y] = navio4[k];
+        } else{
+            printf("Erro: Índice fora dos limites do tabuleiro.\n");
+            return -1;
+        }
+    } 
+  
+    
 
     // Exibição do Tabuleiro
     for (int i = 0; i < 10; i++) {
